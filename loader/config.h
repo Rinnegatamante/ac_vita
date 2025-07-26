1a12d9d15af9c26bd522b57273260c001027159f
Rinnegatamante <rinnegatamante@gmail.com>
2025-07-26 16:45:20 +0200

    Source upload.

---
+++ loader/config.h
@@ -0,0 +1,15 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define LOAD_ADDRESS 0x98000000
+
+#define MEMORY_NEWLIB_MB 256
+#define MEMORY_VITAGL_THRESHOLD_MB 8
+
+#define DATA_PATH "ux0:data/assassins"
+#define SO_PATH DATA_PATH "/" "libassissinscreed.so"
+
+#define SCREEN_W 960
+#define SCREEN_H 544
+
+#endif
